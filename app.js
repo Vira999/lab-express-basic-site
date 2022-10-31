b4a1da6dea7d8407cfb9d6f255fcddea8c779f96
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2022-10-31 20:16:54 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express') // loads the express package
+const app = express() // executes an express server 
+const port = 3000 // defining our server port
+
+app.use(express.static('public')); // configuration step which tells where my static files are located
+
+app.get('/', (req, res) => { // defining my homepage route
+
+res.sendFile(__dirname + '/views/jlohome-page.html')
+})
+
+aapp.get('/about', (request, response, next) => 
+response.sendFile(__dirname + '/views/about.html'));
+
+app.get('/works', (request, response, next) => 
+response.sendFile(__dirname + '/views/works.html'));
+
+
+
+app.listen(port, () => { // allows incoming requests from clients
+  console.log(`Example app listening on port ${port}`)
+})
